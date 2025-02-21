c11142a320818bb2d9bc24c89a9b0f517674bcca
Nushka <anushka.srivastava05@gmail.com>
2025-02-21 17:04:01 +0530

    recommit

---
+++ main.tf
@@ -0,0 +1,8 @@
+resource "azurerm_resource_group" "RG1" {
+  name     = "SwissRG"
+  location = "Central india"
+}
+resource "azurerm_resource_group" "RG2" {
+  name     = "SwissRG2"
+  location = "East US"
+}
---
+++ provider.tf
@@ -0,0 +1,13 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "4.19.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+  subscription_id = "8e88eb9f-15fc-40c4-97dc-d3192d0d47fa"
+}
